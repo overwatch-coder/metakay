575f63176b1dda00a222a0309a3140eb2b1f2a70
Nyamadi Atsu <devbrainy98@gmail.com>
2023-02-01 04:20:33 +0100

    update: changed font sizes for product section on mobile

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,23 +9,23 @@ const Product = ({productImage, slug, productName, price, category}) => {
         </Link>
 
         <div className='bg-gray py-4 text-white flex justify-around'>
-            <FaCartPlus className='cursor-pointer text-sm md:text-base lg:text-xl hover:scale-110' />
+            <FaCartPlus className='cursor-pointer text-base lg:text-xl hover:scale-110' />
 
             <Link to={`/shop/${slug}`} className='hover:scale-110'>
-                <FaEye className='text-sm md:text-base lg:text-xl' />
+                <FaEye className='text-base lg:text-xl' />
             </Link>
         </div>
 
-        <div className='flex space-x-2 text-xs text-gray font-light items-center'>
+        <div className='flex space-x-2 text-gray font-light items-center'>
           {category && category.map((cat, index) => (
               <p key={index} className="after:content-['|'] after:last:content-[''] after:px-[2px] sm:after:px-2 text-sm lg:text-base">{cat}</p>
           ) )}
         </div>
 
         <div className='py-2 flex justify-between items-center font-medium'>
-            <p className='text-xs sm:text-sm lg:text-base'>{productName}</p>
+            <p className='lg:text-base'>{productName}</p>
 
-            <p className='text-xs sm:text-sm lg:text-base'>${price}.00</p>
+            <p className='lg:text-base'>${price}.00</p>
         </div>
     </div> 
   )
