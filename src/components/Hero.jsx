cab661988432e7b706697d146297beea82667a35
Nyamadi Atsu <devbrainy98@gmail.com>
2023-02-05 12:38:53 +0100

    added more height to homepage hero video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import heroVideo from '../assets/hero-2.mp4';
 const Hero = () => {
   return (
     <section>
-        <video autoPlay muted loop className='w-full object-cover sm:h-[130vh]'>
+        <video autoPlay muted loop className='w-full object-cover sm:h-[135vh]'>
             <source src={heroVideo} type='video/mp4' />
         </video>
     </section>
